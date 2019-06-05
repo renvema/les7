6c9f0d1977ecdc28336470ab7ad10c472818b96c
Maryana <renvema@bigmir.net>
2019-06-05 09:46:38 +0300

    les7 commit2

--- src/task/seventh/Main.java
+++ src/task/seventh/Main.java
@@ -2,7 +2,8 @@ package task.seventh;
 
 public class Main {
     public static void main(String[] args) {
-        Cupboard<Integer, Dress> cupboard = new Cupboard<>(1, new Dress("blue", 'M'));
+        Dress dress = new Dress("blue", 'M');
+        Cupboard<Integer, Dress> cupboard = new Cupboard<>(1, dress);
         cupboard.getValue();
         System.out.println(cupboard.getValue().toString());
     }
